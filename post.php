d76d9fccf669f16d5d4522e163fb6b8f1246ae50
sezgin <s.karaduman3880@gmail.com>
2023-01-31 22:43:30 +0100

    Pages for the descriptions of posts

--- post.php
+++ post.php
@@ -13,7 +13,8 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM blogs";
+$post_id = $_GET['id'];
+$sql = "SELECT * FROM blogs WHERE id = $post_id";
 $result = mysqli_query($conn, $sql);
 
 ?>
@@ -30,24 +31,14 @@ $result = mysqli_query($conn, $sql);
 <body class="bg-gray-200">
   <div class="container mx-auto">
     <?php if (mysqli_num_rows($result) > 0): ?>
-      <?php while($row = mysqli_fetch_assoc($result)): ?>
-        <?php 
-            $description = substr($row['description'], 0, 32);
-            if (strlen($row['description']) > 32) {
-                $description .= "...";
-            }
-        ?>
+      <?php $row = mysqli_fetch_assoc($result); ?>
         <div class="bg-white p-5 rounded-lg shadow mt-10">
           <h3 class="text-xl font-bold"><?php echo $row['title']; ?></h3>
-          <p class="text-gray-700"><?php echo $description; ?></p>
-          <a href="post.php?id=<?php echo $row['id']; ?>" class="text-blue-500">Read More</a>
+          <p class="text-gray-700"><?php echo $row['description']; ?></p>
         </div>
-      <?php endwhile; ?>
     <?php else: ?>
-      <p class="text-center">No blog posts found.</p>
+      <p class="text-center">No blog post found.</p>
     <?php endif; ?>
   </div>
-  
-  
 </body>
 </html>

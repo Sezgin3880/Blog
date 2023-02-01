aa09e6fbe1f2f79b61e9054ab2494fbd796a0129
sezgin <s.karaduman3880@gmail.com>
2023-02-01 11:31:49 +0100

    Creating blog works (It gets added to the database)

--- create_post.php
+++ create_post.php
@@ -1,46 +1,62 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "blog_posts";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['submit'])) {
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+
+    $sql = "INSERT INTO blogs (title, description) VALUES ('$title', '$description')";
+    if (mysqli_query($conn, $sql)) {
+        header("Location: blog.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Add Blog Post</title>
-  <!-- Tailwind CSS -->
+  <!-- Tailwind -->
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-200">
-  <div class="container mx-auto mt-10">
-    <h1 class="text-2xl font-bold text-center">Add Blog Post</h1>
-    <form action="add_post.php" method="post" class="bg-white p-5 rounded-lg shadow mt-10">
-      <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="title">
-          Title
-        </label>
-        <input 
-          class="border border-gray-400 p-2 w-full" 
-          type="text" 
-          name="title" 
-          id="title" 
-          required
-        >
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 font-bold mb-2" for="description">
-          Description
-        </label>
-        <textarea 
-          class="border border-gray-400 p-2 w-full" 
-          name="description" 
-          id="description" 
-          rows="5" 
-          required
-        ></textarea>
-      </div>
-      <input 
-        type="submit" 
-        value="Add Blog Post" 
-        class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600"
-      >
-    </form>
+  <div class="container mx-auto">
+    <div class="bg-white p-5 rounded-lg shadow mt-10">
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="mb-5">
+          <label class="block text-gray-700 font-bold mb-2" for="title">
+            Title
+          </label>
+          <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="title" type="text" name="title" required>
+        </div>
+        <div class="mb-5">
+          <label class="block text-gray-700 font-bold mb-2" for="description">
+            Description
+          </label>
+          <textarea class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="description" name="description" rows="5" required></textarea>
+        </div>
+        <div class="flex items-center justify-between">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </body>
-</html>

fdf13ef93221b3bb69ee9ebf76db4dccd1280cfa
sezgin <s.karaduman3880@gmail.com>
2023-02-02 13:31:18 +0100

    Delete button (delete_post.php)

--- blog.php
+++ blog.php
@@ -44,6 +44,7 @@ $result = mysqli_query($conn, $sql);
 
   <div class=" text-right">
     <a href="edit_post.php" class="bg-blue-500 text-white px-3 py-1.5 rounded-lg ml-auto">Edit Post</a>
+    <a href="delete_post.php?id=<?php echo $row['id']; ?>" class="bg-red-500 text-white px-3 py-1.5 rounded-lg">Delete Post</a>
   </div>
 
 
